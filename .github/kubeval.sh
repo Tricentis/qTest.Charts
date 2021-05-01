182a4ecf889e69b5cabea4479673524726807505
brandon <b.yang@ext.tricentis.com>
2021-05-01 13:12:15 -0400

    KUBEVAL_VERSION

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | cut -d '/' -f 2 | uniq)"
 
 # renovate: datasource=github-releases depName=instrumenta/kubeval
-KUBEVAL_VERSION="0.16.1"
+KUBEVAL_VERSION="v0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
