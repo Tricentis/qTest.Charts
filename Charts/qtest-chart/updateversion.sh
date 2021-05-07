b50c2662e3e831de4baeccb3beae2cc02156a485
brandon <b.yang@ext.tricentis.com>
2021-05-07 13:21:35 -0400

    add -i to shell script

--- Charts/qtest-chart/updateversion.sh
+++ Charts/qtest-chart/updateversion.sh
@@ -4,7 +4,7 @@ echo Current version $str1
 minorVS=$((minorVS+1))
 str2="${str1%.*}.$minorVS"
 echo Update to version $str2
-sed "s/version:.*/$str2/g" Chart.yaml
+sed -i "s/version:.*/$str2/g" Chart.yaml
 
 str3="$(grep -w 'appVersion:' Chart.yaml)"
 echo Current appVersion $str3
@@ -13,4 +13,4 @@ minorVS="${minorVS%?}"
 minorVS=$((minorVS+1))
 str4="${str3%.*}.$minorVS\""
 echo Update to appVersion $str4
-sed "s/appVersion:.*/$str4/g" Chart.yaml
+sed -i "s/appVersion:.*/$str4/g" Chart.yaml
