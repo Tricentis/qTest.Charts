4c4242ce9cb5c6d6136b97b897e32191eeb789ec
brandon <b.yang@ext.tricentis.com>
2021-05-07 12:48:02 -0400

    add shell script

---
+++ Charts/qtest-chart/updateversion.sh
@@ -0,0 +1,12 @@
+str1="$(grep -w 'version:' Chart.yaml)"
+minorVS="${str1##*.}"
+minorVS=$((minorVS+1))
+str2="${str1%.*}.$minorVS"
+sed -i "s/version:.*/$str2/g" Chart.yaml
+
+str3="$(grep -w 'appVersion:' Chart.yaml)"
+minorVS="${str3##*.}"
+minorVS="${minorVS%?}"
+minorVS=$((minorVS+1))
+str4="${str3%.*}.$minorVS\""
+sed -i "s/appVersion:.*/$str4/g" Chart.yaml
