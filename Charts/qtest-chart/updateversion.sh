082a63753c6fb1a96850b946c913cc60c969779c
brandon <b.yang@ext.tricentis.com>
2021-05-07 13:14:55 -0400

    shell update

--- Charts/qtest-chart/updateversion.sh
+++ Charts/qtest-chart/updateversion.sh
@@ -1,12 +1,16 @@
 str1="$(grep -w 'version:' Chart.yaml)"
 minorVS="${str1##*.}"
+echo Current version $str1
 minorVS=$((minorVS+1))
 str2="${str1%.*}.$minorVS"
+echo Update to version $str2
 sed "s/version:.*/$str2/g" Chart.yaml
 
 str3="$(grep -w 'appVersion:' Chart.yaml)"
+echo Current appVersion $str3
 minorVS="${str3##*.}"
 minorVS="${minorVS%?}"
 minorVS=$((minorVS+1))
 str4="${str3%.*}.$minorVS\""
+echo Update to appVersion $str3
 sed "s/appVersion:.*/$str4/g" Chart.yaml
